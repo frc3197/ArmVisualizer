1b5df196f5794cfdd0383441640574ec729015a0
spencrr <23708360+spencrr@users.noreply.github.com>
2019-02-20 15:13:51 -0600

    initial commit

---
+++ src/main/java/org/team3197/frc2019/arm_visualizer/App.java
@@ -0,0 +1,9 @@
+package org.team3197.frc2019.arm_visualizer;
+
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+
+@Description(group = "frc.team3197", name = "Good Plugin", version = "0.0.0", summary = "A good plugin")
+public class App extends Plugin {
+  
+}
